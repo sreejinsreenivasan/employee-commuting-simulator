dfdb51a46fc2a4cc9404a1d7e519784e2128d3fe
Dónal Adams <williamdonaladams@gmail.com>
2024-06-07 12:15:49 +0200

    Update dummy text

--- frontend/src/pages/simulator.tsx
+++ frontend/src/pages/simulator.tsx
@@ -1,18 +1,19 @@
 import { Col, Divider, Row } from "antd";
 
 export function SimulatorPage() {
+
   return (<>
     <Row>
       <Col span={6}>
         <h1>Controls</h1>
-        <p>TODO: add controls</p>
+        <p>TODO: add form controls for input parameters (See Scenario in backend/app.py)</p>
       </Col>
       <Col span={1}>
         <Divider type="vertical" style={{ height: '100%' }} />
       </Col>
       <Col span={17}>
         <h1>Effect</h1>
-        <p>TODO: add visualisation of results</p>
+        <p>TODO: add visualisation of Simulation Result from http://localhost:5000/simulate</p>
       </Col>
     </Row>
   </>)
