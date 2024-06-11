505ff499cb873f842eb9481ee52d51e193ecbfc6
Sreejin Sreenivasan <sreejinsreenivasan@Admins-MacBook-Air.local>
2024-06-11 15:09:47 +0530

    feat: integrate api call to backend to fetch simulation results

--- frontend/src/pages/simulator.tsx
+++ frontend/src/pages/simulator.tsx
@@ -1,20 +1,49 @@
 import { Col, Divider, Row } from "antd";
+import CommuteResults from "../components/CommuteResults";
+import CommuteForm from "../components/CommuteForm";
+import { useState } from "react";
+import { SimulationResult } from "../types";
 
 export function SimulatorPage() {
+  const [results, setResults] = useState<SimulationResult | null>(null);
 
-  return (<>
-    <Row>
-      <Col span={6}>
-        <h1>Controls</h1>
-        <p>TODO: add form controls for input parameters (See Scenario in backend/app.py)</p>
-      </Col>
-      <Col span={1}>
-        <Divider type="vertical" style={{ height: '100%' }} />
-      </Col>
-      <Col span={17}>
-        <h1>Effect</h1>
-        <p>TODO: add visualisation of Simulation Result from http://localhost:5000/simulate</p>
-      </Col>
-    </Row>
-  </>)
+  const onFinish = async (values: {
+    oneWayDistance: number;
+    commuteDaysPerYear: number;
+    primaryMode: string;
+    secondaryMode?: string | undefined;
+  }) => {
+    const params = new URLSearchParams({
+      primary_mode: values.primaryMode,
+      one_way_distance: values.oneWayDistance.toString(),
+      secondary_mode: values.secondaryMode || "",
+      commute_days_per_year: values.commuteDaysPerYear.toString(),
+    });
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:5000/simulate?" + params.toString()
+      );
+      const data = await response.json();
+      setResults(data);
+    } catch (error) {
+      console.error("Error fetching simulation results", error);
+    }
+  };
+  return (
+    <>
+      <Row>
+        <Col span={6}>
+          <h1>Controls</h1>
+          <CommuteForm onFinish={onFinish} />
+        </Col>
+        <Col span={1}>
+          <Divider type="vertical" style={{ height: "100%" }} />
+        </Col>
+        <Col span={17}>
+          <h1>Effect</h1>
+          {results && <CommuteResults results={results} />}
+        </Col>
+      </Row>
+    </>
+  );
 }
